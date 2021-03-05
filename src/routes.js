1612438fdeb2ab17fb4e805e37dc842c1792c773
gstimm <contato.gabrieltimm@gmail.com>
2021-03-05 16:56:13 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => { return res.json({ message: 'Rota GET'})});
+
+export default router;
---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express, { json } from 'express';
+import routes from './routes.js';
+
+const app = express();
+
+app.use(json());
+app.use(routes);
+
+app.listen(3333, () => console.log('Server started!'));
